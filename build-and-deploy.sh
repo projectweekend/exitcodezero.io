5f25424cb1e7ea66d7950e4f70e4e96e80913edc
Brian Hines <brian@projectweekend.net>
2018-01-01 10:46:49 -0600

    Improve deploy script

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+HOME_DIR="/home/brian" && \
+ECZ_DIR=$(pwd) && \
+RESUME_DIR="$HOME_DIR/Dropbox/resume" && \
+RESUME_FILE="resume.html" && \
+RESUME_FILE_PATH="$RESUME_DIR/$RESUME_FILE" && \
+S3_BUCKET="s3://exitcodezero.io" && \
+cd $RESUME_DIR && \
+resume export $RESUME_FILE --format=html --theme=elegant && \
+cd $ECZ_DIR && \
 hugo && \
-cp /home/brian/Dropbox/resume/resume.html public/resume.html && \
+cp $RESUME_FILE_PATH public/$RESUME_FILE && \
 cd public && \
-aws s3 sync . s3://exitcodezero.io --delete
+aws s3 sync . $S3_BUCKET --delete
